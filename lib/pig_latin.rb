22b32f790955555ad884dbe9f310349116a02674
Kristin Brewer_Lowe and Rohan Moore <kristin@>
2018-11-15 14:52:01 -0800

    meet spec 4

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -4,9 +4,31 @@ def pig_latin(word)
   vowels = ["a", "e", "i", "o", "u", "y"]
   pig_latin = ""
   first_letter = word[0,1]
+  index_of_first_vowel = first_vowel(word)
   if vowels.include?(first_letter)
     pig_latin += "#{word}way"
   else
-    pig_latin += "#{word[1..-1]}#{word[0,1]}ay"
+    pig_latin += "#{word[index_of_first_vowel..-1]}#{word[0,index_of_first_vowel]}ay"
   end
+end
+
+def first_vowel(word)
+  vowels = ["a", "e", "i", "o", "u", "y"]
+  index = 0
+  first_vowel = 0
+  while index < word.length
+    vowels.each do |vowel|
+      if word[index] == vowel
+        first_vowel = index
+        break
+      end
+    end
+    index += 1
+  end
+  first_vowel
+  # vowel_index = []
+  # vowels.each do |vowel|
+  #   vowel_index.push(word.index(vowel))
+  # end
+  # vowel_index[0]
 end
