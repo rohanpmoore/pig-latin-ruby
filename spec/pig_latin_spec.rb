6cadf0b13f87e54a21ea4ec90ccf85e9a8bf0ac5
Kristin Brewer_Lowe and Rohan Moore <kristin@>
2018-11-15 14:16:17 -0800

    Add spec 2, fails

--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -2,7 +2,10 @@ require 'rspec'
 require 'pig_latin'
 
 describe('pig_latin') do
-  it('will add "way" to the letter "a"') do
+  it('will add "way" to any vowel') do
     expect(pig_latin("a")).to(eq("away"))
+  end
+  it('will add "way" to the end of any word beginning with a vowel') do
+    expect(pig_latin("email")).to(eq("emailway"))
   end
 end
