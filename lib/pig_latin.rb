68f19a282538addf0644b9a34c3e4106554fb52c
Kristin Brewer_Lowe and Rohan Moore <kristin@>
2018-11-15 14:18:56 -0800

    pass spec 2

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -3,7 +3,8 @@ require 'pry'
 def pig_latin(word)
   vowels = ["a", "e", "i", "o", "u", "y"]
   pig_latin = ""
-  if vowels.include?(word)
+  first_letter = word[0,1]
+  if vowels.include?(first_letter)
     pig_latin += "#{word}way"
   end
 end
