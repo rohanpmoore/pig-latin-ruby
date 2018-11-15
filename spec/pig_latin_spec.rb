0a3a088cf5a8b36e75f95a3c43bad6eadb524af7
Kristin Brewer_Lowe and Rohan Moore <rohan@>
2018-11-15 14:22:08 -0800

    add spec 3, fails

--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -8,4 +8,7 @@ describe('pig_latin') do
   it('will add "way" to the end of any word beginning with a vowel') do
     expect(pig_latin("email")).to(eq("emailway"))
   end
+  it('will move a single consonant from the front to end of a word and add "ay"') do
+    expect(pig_latin("pig")).to(eq("igpay"))
+  end
 end
